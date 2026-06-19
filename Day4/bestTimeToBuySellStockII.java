1b6d43af7b7effd4125261f4735bbb3eacb5f323
kanis <kkppyy13@gmail.com>
2026-06-19 18:33:06 +0530

    Day4

---
+++ Day4/bestTimeToBuySellStockII.java
@@ -0,0 +1,17 @@
+class Solution {
+    public class bestTimeToBuySellStockII {
+        int profit = 0;
+
+        for(
+        int i = 1;
+        i<prices.length;i++)
+
+        {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+}
---
+++ Day4/bestTimeToSellStock.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int bestTimeToSellStock(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+}
